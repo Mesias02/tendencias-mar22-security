4d1aa4c388988f5a49f5e5c9898a9d1950f36a20
Marco Guaman <--add>
2022-05-30 08:16:57 -0500

    add cors cofiguration

--- src/main/kotlin/com/empresa/security/controller/UserController.kt
+++ src/main/kotlin/com/empresa/security/controller/UserController.kt
@@ -5,13 +5,11 @@ import com.empresa.security.service.UserService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/users")
+@CrossOrigin(methods = [RequestMethod.GET, RequestMethod.POST, RequestMethod.PATCH, RequestMethod.PUT], origins = ["http://localhost:3000"] )
 class UserController {
 
     @Autowired
--- src/main/kotlin/com/empresa/security/model/User.kt
+++ src/main/kotlin/com/empresa/security/model/User.kt
@@ -3,7 +3,7 @@ package com.empresa.security.model
 import javax.persistence.*
 
 @Entity
-@Table(name = "users    ")
+@Table(name = "users")
 class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
